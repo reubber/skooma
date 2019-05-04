b9b45495f54388ab15e3b1213513f4ecf2bec3d7
rBBr <reubbers@gmail.com>
2019-05-04 19:52:40 -0300

    skooma 1.0.0 --version

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import "./App.scss";
+import { Container, Section } from 'rbx';
+import Header from "./components/header";
+import Routes from './routes';
+const App = () => (
+  <div>
+    <Header />  
+    <Section>
+      <Container>
+        <Routes />
+      </Container>
+    </Section>
+  </div>
+)
+ 
+export default App;
