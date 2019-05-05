90b4f683114692cf984fb9c251f15cd288a79dd7
rBBr <reubbers@gmail.com>
2019-05-05 20:58:14 -0300

    skooma 1.3.3 --version

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,6 +1,12 @@
 import { combineReducers } from "redux";
 import RestaurantsReducer from "./restaurantsReduces";
+import ModalReducer from "./modalReducer";
+import AddressReducer from "./addressReduces";
+import NewOrderReducer from "./newOrderReducer";
 
 export default combineReducers({
-  restaurantsState: RestaurantsReducer	
+  restaurantsState: RestaurantsReducer,
+  modalState: ModalReducer,
+  addressState: AddressReducer,
+  newOrderState: NewOrderReducer
 })
