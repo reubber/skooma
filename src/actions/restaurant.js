7baeb04ca64e97d31b45deeff2c1a643e44fc8bd
rBBr <reubbers@gmail.com>
2019-05-04 23:26:33 -0300

    skooma 1.1.2 --version
    min 56;
    
    -pendencias:
    :criar modals

--- src/actions/restaurant.js
+++ src/actions/restaurant.js
@@ -1,7 +1,19 @@
-import restaurants from "../reducers/restaurants";
 import { LOAD_RESTAURANTS } from "./action_types";
+import api from "../services/api";
  
-export const loadRestaurants = () => ({
-  type: LOAD_RESTAURANTS,
-  restaurants: restaurants
-});
+export const loadRestaurants = (category = null) => async (dispatch) => {
+  let response = await api.loadRestaurants(category);
+  dispatch({
+    type: LOAD_RESTAURANTS,
+    restaurants: response.data.restaurants
+  });
+  
+}
+
+export const searchRestaurants = (search) => async (dispatch) => {
+  let response = await api.searchRestaurants(search)
+  dispatch({
+    type: LOAD_RESTAURANTS,
+    restaurants: response.data.restaurants
+  });
+}
